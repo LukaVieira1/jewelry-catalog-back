20cb3e21bddbc34921322cf0a7389861354c7d69
LukaVieira1 <lukavieira14@gmail.com>
2022-05-04 00:20:56 -0300

    configuração de controllers e routes, necessario ajuste do upload da imagem

---
+++ src/middleware/upload.js
@@ -0,0 +1,15 @@
+import multer from "fastify-multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/");
+  },
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
+
+    cb(null, file.fieldname + "-" + Date.now() + extension);
+  },
+});
+
+export const upload = multer({ storage });
