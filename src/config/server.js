7d7fc1f10bad4a6df958a72f495b4e1d669c38fb
Luka Vieira <lukavieira14@gmail.com>
2022-05-04 10:09:54 -0300

    wip: create error

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,11 @@
 import fastify from "fastify";
 import helmet from "fastify-helmet";
+import multer from "fastify-multer";
+import fastifyStatic from "fastify-static";
+import path from "path";
 import cors from "fastify-cors";
+
+const __dirname = path.resolve();
 
 import routes from "../routes/routes.js";
 
@@ -8,11 +13,17 @@ const app = fastify({
   logger: true,
 });
 
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/",
+});
+
 app.register(cors, {
   origin: "*",
   methods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"],
 });
 
+app.register(multer.contentParser);
 app.register(helmet);
 app.register(routes);
 
