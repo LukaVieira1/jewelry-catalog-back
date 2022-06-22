1f8c5e119558e5ba648b4a629a590bacf9b98417
Luka Vieira <lukavieira@MacBook-Air-de-Luka.local>
2022-06-22 14:58:00 -0300

    fix: heroku doesn't create public/images

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images/");
+    cb(null, "public/");
   },
   filename: (req, file, cb) => {
     const extension = path.extname(file.originalname);
